deeca631992fee51215e42d353e77d7a97a29bb0
jose malaspina <majocalon@gmail.com>
2023-02-27 20:43:51 -0300

    ultima

---
+++ js/mostrar.js
@@ -0,0 +1,39 @@
+    document.getElementById('info').addEventListener('click', function(){
+        if (document.getElementById('info-detalle').style.display==='none'){
+             document.getElementById('info-detalle').style.display='block';
+        }
+        else{
+             document.getElementById('info-detalle').style.display='none';
+        }
+        })
+    
+
+        document.getElementById('descripcion').addEventListener('click', function(){
+            if (document.getElementById('info-descripcion').style.display==='none'){
+                 document.getElementById('info-descripcion').style.display='block';
+            }
+            else{
+                 document.getElementById('info-descripcion').style.display='none';
+            }
+            })
+        
+
+            document.getElementById('experiencia').addEventListener('click', function(){
+                if (document.getElementById('info-experiencia').style.display==='none'){
+                     document.getElementById('info-experiencia').style.display='block';
+                }
+                else{
+                     document.getElementById('info-experiencia').style.display='none';
+                }
+                })
+            
+
+                document.getElementById('habilidades').addEventListener('click', function(){
+                    if (document.getElementById('info-habilidades').style.display==='none'){
+                         document.getElementById('info-habilidades').style.display='block';
+                    }
+                    else{
+                         document.getElementById('info-habilidades').style.display='none';
+                    }
+                    })
+                
